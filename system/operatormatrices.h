95ac70311653113c0d567cf4071a351c6bd514d2
David Bauch <davidbauch1@web.de>
2019-10-23 18:13:17 +0200

    Added new function to project operators onto maps

--- system/operatormatrices.h
+++ system/operatormatrices.h
@@ -17,6 +17,7 @@ class OperatorMatrices_Parent {
     // Operator functions
     template <class M>
     static M expand_photonic_operator( const M &op, int N ) {
+        std::assert( op.cols() == op.rows() && "Rows must be equal to cols" );
         M ret = M::Zero( op.rows() * N, op.rows() * N );
         for ( int i = 0; i < op.cols(); i++ )
             for ( int j = 0; j < op.rows(); j++ )
@@ -28,6 +29,7 @@ class OperatorMatrices_Parent {
     }
     template <class M>
     static M expand_atomic_operator( const M &op, int N ) {
+        std::assert( op.cols() == op.rows() && "Rows must be equal to cols" );
         M ret = M::Zero( op.rows() * ( N + 1 ), op.rows() * ( N + 1 ) );
         for ( int n = 0; n <= N; n++ )
             for ( int i = 0; i < op.cols(); i++ )
@@ -37,7 +39,7 @@ class OperatorMatrices_Parent {
     }
     template <class M>
     static M tensor( const M &a, const M &b ) {
-        assert( a.rows() == a.cols() && b.rows() == b.cols() );
+        std::assert( a.rows() == a.cols() && b.rows() == b.cols() );
         M ret = M::Zero( a.cols() * b.cols(), a.rows() * b.rows() );
         for ( int i = 0; i < a.cols(); i++ )
             for ( int j = 0; j < a.rows(); j++ )
@@ -47,9 +49,29 @@ class OperatorMatrices_Parent {
                     }
         return ret;
     }
+    template <class M>
+    static M create_photonic_operator( const int type, const int maxPhotons ) {
+        M ret = M::Zero( maxPhotons + 1, maxPhotons + 1 );
+        for ( int i = 0; i < maxPhotons; i++ ) {
+            if ( type == OPERATOR_PHOTONIC_CREATE )
+                ret( i + 1, i ) = sqrt( i + 1 );
+            if ( type == OPERATOR_PHOTONIC_ANNIHILATE )
+                ret( i, i + 1 ) = sqrt( i + 1 );
+        }
+        return ret;
+    }
+    template <class M>
+    static M map_operator( const M &op, const M &map ) {
+        std::assert( op.cols() == op.rows() && map.cols() == map.rows() && "Rows must be equal to cols" );
+        M ret = M::Zero( op.rows() * ( map.rows() ), op.cols() * ( map.cols() ) );
+        for ( int i = 0; i < op.cols(); i++ )
+            for ( int j = 0; j < op.rows(); j++ )
+                for ( int n = 0; n < map.cols(); n++ )
+                    for ( int m = 0; m < map.rows(); m++ )
+                        ret( i * map.cols() + n, j * map.rows() + m ) = op( i, j ) * map( n, m );
+        return ret;
+    }
     // To be called from child constructor
     void init( const Parameters &p );
     // Basic Operator functions:
-    MatrixXcd create_photonic_operator( const int type, const int maxPhotons );
-    //MatrixXcd create_electronic_operator(const int type, const int maxStates);
 };
