4db97341eff3c30a56cc032ed28477b7b599fa88
David Bauch <davidbauch1@web.de>
2019-11-08 18:21:30 +0100

    Added static function to fileoutput multiple pulses into single file

--- pulse.cpp
+++ pulse.cpp
@@ -55,7 +55,7 @@ void Pulse::fileOutput( std::string filepath ) {
         return;
     }
     for ( long unsigned int i = 0; i < timearray.size() - steps.size(); i += steps.size() ) {
-        std::fprintf( pulsefile, "%.10e\t%.10e\n", timearray.at( i ), std::abs( pulsearray.at( i ) ) );
+        fmt::print( pulsefile, "{:.10e}\t{:.10e}\n", timearray.at( i ), std::abs( pulsearray.at( i ) ) );
     }
     std::fclose( pulsefile );
 }
@@ -66,6 +66,7 @@ void Pulse::Inputs::add( double _center, double _amp, double _sigma, double _ome
     sigma.emplace_back( _sigma );
     omega.emplace_back( _omega );
     type.emplace_back( _type );
+    logs.level2( "Added Pulse with parameters: center = {}, amp = {}, sigma = {}, omega = {}, type = {}. No filter was used.\n", _center, _amp, _sigma, _omega, _type );
 }
 
 void Pulse::Inputs::add( std::vector<double> &_center, std::vector<double> &_amp, std::vector<double> &_sigma, std::vector<double> &_omega, std::vector<std::string> &_type ) {
@@ -79,6 +80,25 @@ void Pulse::Inputs::add( std::vector<double> &_center, std::vector<double> &_amp
         sigma.emplace_back( _sigma.at( i ) );
         omega.emplace_back( _omega.at( i ) );
         type.emplace_back( _type.at( i ) );
+        logs.level2( "Added Pulse with parameters: center = {}, amp = {}, sigma = {}, omega = {}, type = {}. No filter was used.\n", _center.at( i ), _amp.at( i ), _sigma.at( i ), _omega.at( i ), _type.at( i ) );
+    }
+}
+void Pulse::Inputs::add( std::vector<double> &_center, std::vector<double> &_amp, std::vector<double> &_sigma, std::vector<double> &_omega, std::vector<std::string> &_type, std::vector<std::string> &_filter, std::string to_match ) {
+    if ( !( _center.size() == _amp.size() && _sigma.size() == _omega.size() && _amp.size() == _sigma.size() && _sigma.size() == _type.size() && _type.size() == _filter.size() ) ) {
+        logs.level2( "Input arrays don't have the same length! No Vectors are created, initializing pulse will fail!\n" );
+        return;
+    }
+    for ( int i = 0; i < (int)_amp.size(); i++ ) {
+        if ( to_match.compare( _filter.at( i ) ) == 0 ) {
+            center.emplace_back( _center.at( i ) );
+            amp.emplace_back( _amp.at( i ) );
+            sigma.emplace_back( _sigma.at( i ) );
+            omega.emplace_back( _omega.at( i ) );
+            type.emplace_back( _type.at( i ) );
+            logs.level2( "Added Pulse with parameters: center = {}, amp = {}, sigma = {}, omega = {}, type = {}. Filter {} was used.\n", _center.at( i ), _amp.at( i ), _sigma.at( i ), _omega.at( i ), _type.at( i ), to_match );
+        } else {
+            logs.level2( "Failed to add Pulse with parameters: center = {}, amp = {}, sigma = {}, omega = {}, type = {}. Mismatched Filter {} was used.\n", _center.at( i ), _amp.at( i ), _sigma.at( i ), _omega.at( i ), _type.at( i ), to_match );
+        }
     }
 }
 
@@ -107,4 +127,22 @@ dcomplex Pulse::get( double t, bool force_evaluate ) {
 
 void Pulse::log() {
     logs.level2( "Pulse evaluations/returns: {}/{}\n", counter_evaluated, counter_returned );
+}
+
+static void fileOutput( std::string filepath, std::vector<Pulse> pulses ) {
+    FILE *pulsefile = std::fopen( filepath.c_str(), "w" );
+    if ( !pulsefile ) {
+        logs.level2( "Failed to open outputfile for Pulse!\n" );
+        return;
+    }
+    for ( long unsigned int i = 0; i < pulses.at( 0 ).timearray.size() - pulses.at( 0 ).steps.size(); i += pulses.at( 0 ).steps.size() ) {
+        fmt::print( pulsefile, "{:.8e}\t", pulses.at( 0 ).timearray.at( i ) );
+        for ( long unsigned int p = 0; p < pulses.size(); p++ ) {
+            if ( i < pulses.at( p ).timearray.size() ) {
+                fmt::print( pulsefile, "{:.8e}\t", std::abs( pulses.at( p ).pulsearray.at( i ) ) );
+            }
+        }
+        fmt::print( pulsefile, "\n" );
+    }
+    std::fclose( pulsefile );
 }
--- pulse.h
+++ pulse.h
@@ -16,21 +16,23 @@ class Pulse {
         Inputs( double t_start, double t_end, double t_step, int order ) : t_start( t_start ), t_end( t_end ), t_step( t_step ), order( order ) {}
         void add( double _center, double _amp, double _sigma, double _omega, std::string _type );
         void add( std::vector<double> &_center, std::vector<double> &_amp, std::vector<double> &_sigma, std::vector<double> &_omega, std::vector<std::string> &_type );
+        void add( std::vector<double> &_center, std::vector<double> &_amp, std::vector<double> &_sigma, std::vector<double> &_omega, std::vector<std::string> &_type, std::vector<std::string> &_filter, std::string to_match );
     };
 
    private:
+    int size;
+    int counter_evaluated, counter_returned;
+    void generate( Parameters &p );
+
+   public:
     // Pulse values
     std::vector<dcomplex> pulsearray;
     // Saving the corresponding t doesn't hurt
     std::vector<double> timearray;
-    // Helper variables/functions
-    void generate( Parameters &p );
-    int size;
-    int counter_evaluated, counter_returned;
     std::vector<double> steps;
+    // Helper variables/functions
     Inputs inputs;
 
-   public:
     Pulse(){};
     Pulse( Inputs &inputs );
     void generate();
@@ -38,4 +40,5 @@ class Pulse {
     dcomplex evaluate( double t );
     dcomplex get( double t, bool force_evaluate = false );
     void log();
+    static void fileOutput( std::string filepath, std::vector<Pulse> pulses );
 };
