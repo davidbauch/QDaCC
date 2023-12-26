a0c134efb2aba52f4b381cf704321ed55224bb5b
BuildTools <unconfigured@null.spigotmc.org>
2023-12-26 17:26:22 +0100

    Changed Version to QDaCC 0.1.5

--- include/system/parameters.h
+++ include/system/parameters.h
@@ -7,7 +7,7 @@
 #include "system/parameter.h"
 
 #ifndef GLOBAL_PROGRAM_VERSION
-#define GLOBAL_PROGRAM_VERSION "4.0.5"
+#define GLOBAL_PROGRAM_VERSION "0.1.5"
 #endif
 #ifndef GLOBAL_PROGRAM_LASTCHANGE
 #define GLOBAL_PROGRAM_LASTCHANGE ""
